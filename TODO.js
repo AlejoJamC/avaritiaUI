6b2531eb08d695594db6049c60f995f4b0a4d03b
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-10-30 21:04:42 -0500

    added global todo file

--- TODO.js
+++ TODO.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
 // Views
 // Partials
 // head
